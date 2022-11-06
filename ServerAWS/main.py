8f4cf32b626cffef9a8ebad29a424936b8d82ab8
ashtonhess <ashton.hess@yahoo.com>
2022-11-06 04:06:41 -0600

    Finishing up ServerAWS. All instances should be able to run this forever now. Need to test recovery method when connection is lost more.

--- ServerAWS/main.py
+++ ServerAWS/main.py
@@ -16,6 +16,18 @@ import websockets
 # print(elapsed_time)
 aws_location = 'us-east-1-NVirginia'
 
+class MainServerMessage:
+    def __init__(self, current_time, aws_location):
+        self.current_time = current_time
+        self.exchange_avg_latency = []
+        self.aws_location = aws_location
+
+class ExchAvgData:
+    def __init__(self, exchange):
+        self.exchange = exchange
+        self.sum = 0
+
+
 class ExchTime:
     def __init__(self, exchange, latency):
         self.exchange = exchange
@@ -27,36 +39,34 @@ class ExchangeCCTX:
         self.exchange = exchange
         self.ccxtObj = ccxtObj
 
-        # print(exch.exchangeName + ExchTime)
 
+exchanges = ['coinbase', 'coinbasepro', 'binance', 'binanceus', 'gemini', 'kraken']
 
-
-allTimes = []
+# Array to hold CCXT objects for each exchange.
 exchangeCCXTObjs = []
 
-coinbaseObj = ExchangeCCTX('coinbase', ccxt.coinbase())
+coinbaseObj = ExchangeCCTX(exchanges[0], ccxt.coinbase())
 exchangeCCXTObjs.append(coinbaseObj)
-coinbaseproObj = ExchangeCCTX('coinbasepro', ccxt.coinbasepro())
+coinbaseproObj = ExchangeCCTX(exchanges[1], ccxt.coinbasepro())
 exchangeCCXTObjs.append(coinbaseproObj)
-binanceObj = ExchangeCCTX('binance', ccxt.binance())
+binanceObj = ExchangeCCTX(exchanges[2], ccxt.binance())
 exchangeCCXTObjs.append(binanceObj)
-binanceusObj = ExchangeCCTX('binanceus', ccxt.binanceus())
+binanceusObj = ExchangeCCTX(exchanges[3], ccxt.binanceus())
 exchangeCCXTObjs.append(binanceusObj)
-geminiObj = ExchangeCCTX('gemini', ccxt.gemini())
+geminiObj = ExchangeCCTX(exchanges[4], ccxt.gemini())
 exchangeCCXTObjs.append(geminiObj)
-krakenObj = ExchangeCCTX('kraken', ccxt.kraken())
+krakenObj = ExchangeCCTX(exchanges[5], ccxt.kraken())
 exchangeCCXTObjs.append(krakenObj)
 
-# exchangeCCXTObjs = [ccxt.coinbase(),ccxt.coinbasepro(),ccxt.binance(),ccxt.binanceus(),ccxt.gemini(),ccxt.kraken()]
-# times = []
-# testcounter = 0
+
 async def run():
     async with websockets.connect("ws://cuppong.hessdevelopments.com:11328/mainserver") as websocket:
+        # try:
+        main_counter = 0
+        all_times = []
         while True:
             times = []
-            testcounter = 0
             counter = 0
-            print(testcounter)
             for exch in exchangeCCXTObjs:
                 if counter == 0 or counter == 4:
                     start_time = time.perf_counter()
@@ -68,36 +78,69 @@ async def run():
                     end_time = time.perf_counter()
                 counter += 1
                 elapsed_time = end_time - start_time
-                object = ExchTime(exch.exchange, elapsed_time)
-                object.current_time = time.time()
-                object.location = aws_location
-                times.append(object)
-                print("Exchange: "+object.exchange)
-                print("Latency: "+str(object.latency))
-                print("current_time: "+str(object.current_time))
-                print(response)
-                print()
-                print(times)
-                print()
-                testcounter += 1
+                obj = ExchTime(exch.exchange, elapsed_time)
+                obj.current_time = time.time()
+                obj.location = aws_location
+                times.append(obj)
+                # print("Exchange: " + obj.exchange)
+                # print("Latency: " + str(obj.latency))
+                # print("current_time: " + str(obj.current_time))
+                # print(response)
+                # print()
+                # print(times)
+                # print()
+                all_times += times
+            # json_string = json.dumps([ob.__dict__ for ob in times])
+            # print("HERE HERE HERE")
+            # print(json_string)
+            main_counter += 1
+            if main_counter != 0 and main_counter % 10 == 0:
+                print("ITERATION: " + str(main_counter))
+                exch_avg_objs = []
+                for exch in exchanges:
+                    obj = ExchAvgData(exch)
+                    exch_avg_objs.append(obj)
+                for exch_time_obj in all_times:
+                    for i in range(0, 6):
+                        # print("i: "+str(i))
+                        if exch_time_obj.exchange == exchanges[i]:
+                            # print()
+                            # print(exch_time_obj.exchange)
+                            # print(exchanges[i])
+                            # print()
+                            exch_avg_objs[i].sum += exch_time_obj.latency
+                            # print(exch_avg_objs[i].sum)
+                    main_server_message_obj = MainServerMessage(time.time(), aws_location)
+                i = 0
+                for exch in exchanges:
+                    print("HERE DSLFKJLDSGNSD")
+                    print(exch_avg_objs[i].sum / 10)
+                    main_server_message_obj.exchange_avg_latency.append(exch_avg_objs[i].sum / 10)
+                    print(main_server_message_obj.exchange_avg_latency[i])
+                    print("HERE DSLFKJLDSGNSD")
+                    i+=1
+                # json_string = json.dumps([obj.__dict__ for obj in main_server_message_obj])
+                json_string = json.dumps(main_server_message_obj.__dict__)
+                await websocket.send(json_string)
+                all_times = []
+        # except websockets.ConnectionClosedError:
+        #     print("EXCEPTION")
+        #     # websocket = websockets.connect("ws://cuppong.hessdevelopments.com:11328/mainserver")
+        #     asyncio.run(run())
 
-            #testJSONstr = json.dumps(times[0].__dict__)
-            #print("HEREHERE")
-            #print(testJSONstr)
-            json_string = json.dumps([ob.__dict__ for ob in times])
-            print("HERE HERE HERE")
-            print(json_string)
-            await websocket.send(json_string)
-                # jsonstr1 = json.dumps(s1.__dict__)
-                # for obj in times:
+    # class MainServerMessage:
+    #     def __init__(self, current_time):
+    #         self.current_time = current_time
+    #         self.exchange_avg_latency = []
+
 
     # Send times array to main server
     # # If needed, add times to alltimes (memory issues...)
-    # times = []
-    # coinbaseTimeToJSON = {
-    #     "Exchange": "coinbase",
-    #     "Latency": "CEO",
-    #     "company_name": "Bell System",
-    # }
 
-asyncio.run(run())
+while True:
+    try:
+        asyncio.run(run())
+    except websockets.ConnectionClosedError:
+        print("EXCEPTION")
+        # websocket = websockets.connect("ws://cuppong.hessdevelopments.com:11328/mainserver")
+        asyncio.run(run())
